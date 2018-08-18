ac01669bbfb06b57d30fd834c0c186147d1cdbc8
JamesDaniel <jamesmcgarr@protonmail.com>
2018-08-19 00:54:34 +0100

    simplified DHT22 script.

--- script-v2.ino
+++ script-v2.ino
@@ -26,20 +26,9 @@ void loop()
   sensors_event_t event;  
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
-    Serial.println("Error reading temperature!");
+    Serial.println("0.00");
   } else {
-    Serial.print("Temperature: ");
-    Serial.print(event.temperature);
-    Serial.println(" *C");
-  }
-//   Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println("Error reading humidity!");
-  } else {
-    Serial.print("Humidity: ");
-    Serial.print(event.relative_humidity);
-    Serial.println("%");
+    Serial.println(event.temperature, 1);
   }
 }
 
